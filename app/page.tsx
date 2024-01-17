7f7cbb47ef20a02048ba05d2e8a89b7b10c20d70
Travier.Thomas <travier.thomas@cci.com>
2024-01-17 21:46:04 +0100

    ajout de intro.tsx et de l'image de profil

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import Intro from "@/components/intro"
 
 export default function Home() {
   return (
     <main className="">
-      
+      <Intro />
     </main>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -6,8 +6,16 @@ export default function Intro() {
     <section>
         <div className="flex items-center justify-center">
             <div>
-                <Image src="@/public/moi.jpg" alt="Thomas Travier">
-
+                <Image
+                  src="/moi.jpg"  alt="Thomas Travier" 
+                  width="192" 
+                  height="192" 
+                  quality="95"
+                  priority={true}
+                  className="h-24 w-24 rounded-full border-[0.35rem]
+                  border-white shadow-xl"
+                  >
+                   
                 </Image>
              </div>    
           
