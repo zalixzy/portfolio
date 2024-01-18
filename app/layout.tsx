29496f7a57f2761127c8b2d1f315743751fea453
Travier.Thomas <travier.thomas@cci.com>
2024-01-18 23:08:17 +0100

    avancement dans l'image + description profil

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,11 @@ export default function RootLayout({
   return (
     // Couleur du background du site et couleurs permettant de styliser le background
     <html lang="en">
-      <body className={`${inter.className} bg-gray-800 text-gray-950 relative h-[5000px] pt-28
+      <body className={`${inter.className} bg-sky-100 text-gray-950 relative h-[5000px] pt-28
        sm:pt-36 `}>{children}
-        <div className="bg-[#164e63] absolute top-[-6rem] right-[11rem] h-[31.25rem] 
+        <div className="bg-blue-400 absolute top-[-6rem] right-[11rem] h-[31.25rem] 
         w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#0c4a6e] absolute top-[-1rem] left-[-35rem] h-[31.25rem] 
+        <div className="bg-gray-400  absolute top-[-1rem] left-[-35rem] h-[31.25rem] 
         w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem]
         xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <Header/>
--- components/intro.tsx
+++ components/intro.tsx
@@ -3,14 +3,17 @@
 import React from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import { BsArrowRight, BsLinkedin } from "react-icons/bs"
+import { HiDownload } from "react-icons/hi"
 
 import { useTranslation } from 'react-i18next';
+import Link from 'next/link';
 
 
   export default function Intro() {
     const { t } = useTranslation();
     return (
-      <section>
+      <section className="mb-28 max-w-[50rem] text-center sm:mb-0 ">
           <div className="flex items-center justify-center">
               <div className="">
                 <motion.div 
@@ -34,23 +37,42 @@ import { useTranslation } from 'react-i18next';
             
           </div>
           
-          <motion.p className="text-white text-center"
-          initial ={{opacity:0, scale: 0}}
-          animate= {{opacity:1, scale:1}}>
-            <div>
-              <span className="font-bold">Bonjour, Je m'appelle Thomas
-                {/* {t('Intro1')} */}
-              </span>
-            </div> 
-            <div>
+          <motion.p className="text-gray-950  mb-10 mt-4 px-4 font-medium text-2xl !leading-[1.5]
+           sm:text-2xl "
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            >
+            <span className="font-bold">
+              Bonjour, Je m'appelle Thomas
+              {/* {t('Intro1')} */}
+            </span>
+            <br />
             {/* {t('Intro2')} */}
-            Je suis un{" "}
-              <span className="font-bold">étudiant
-                {/* {t('Intro3')} */}
-              </span>  passionné par l'informatique
-              {/* {t('Intro4')} */}
-            </div>
+            {" "}Je suis un{" "}
+            <span className="font-bold">
+              étudiant
+              {/* {t('Intro3')} */}
+            </span>{" "}
+            passionné par l'informatique
+            {/* {t('Intro4')} */}
           </motion.p>
+          <div>
+            <Link className="bg-gray-950 text-white px-7 py-3 flex items-center gap-2 
+            rounded-full
+            " href="#contact">Me contacter <BsArrowRight />
+            </Link>
+            <a className="bg-gray-200 text-gray-950px-7 py-3 flex items-center gap-2 
+            rounded-full
+            ">Télécharger CV <HiDownload />
+            </a>
+            <a className="bg-gray-950 text-gray-100 p-4 flex items-center gap-2 
+            rounded-full">
+              <BsLinkedin />
+
+            </a>
+
+          </div>
+
          
 
       </section>
