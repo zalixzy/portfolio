d36e005931014451277c54d66ba22725ddc9f57c
Travier.Thomas <travier.thomas@cci.com>
2024-01-19 22:46:40 +0100

    ajout de la page mes projets

---
+++ components/project.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import SectionHeading from './section-heading';
+
+
+export default function Projects() {
+  return (
+    <section>
+        <SectionHeading>Mes projets</SectionHeading>
+        <div>
+            
+        </div>
+
+    </section>
+  )
+}
