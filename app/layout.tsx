e67d0fd9ccb107aed79c10bb4f8b7979461d111c
Travier.Thomas <travier.thomas@cci.com>
2024-01-21 00:39:41 +0100

    stylisation du footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
         <div className="bg-gray-400  absolute top-[-1rem] left-[-35rem] h-[31.25rem] 
         w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem]
         xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+
+        <div className="bg-gray-400 absolute bottom-[-6rem] right-[11rem] h-[10rem] 
+        w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-blue-400  absolute bottom-[-1rem] left-[-35rem] h-[10rem] 
+        w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem]
+        xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <LanguageSelector/>
         <Header/>
         <Footer/>
