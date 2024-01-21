778793fc08352d26aa69fab28cf77fb05bd83c63
Travier.Thomas <travier.thomas@cci.com>
2024-01-21 13:30:53 +0100

    oui

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html className="!scroll-smooth" lang="en">
       <body className={`${inter.className} bg-sky-100 text-gray-950 relative h-[5000px] pt-28
        sm:pt-36 `}>
-        {children}
+        
         <div className="bg-blue-400 absolute top-[-6rem] right-[11rem] h-[31.25rem] 
         w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-gray-400  absolute top-[-1rem] left-[-35rem] h-[31.25rem] 
@@ -37,9 +37,12 @@ export default function RootLayout({
         xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <LanguageSelector/>
         <Header/>
+        {children}
         <Footer/>
+        
         <Toaster position="top-right"/>
         <SpeedInsights/>
+        
         {/* {children} */}
       </body>
     </html>
