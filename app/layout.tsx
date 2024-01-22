578bc5dfbd9e22f21955655d1965556f983d44ec
Travier.Thomas <travier.thomas@cci.com>
2024-01-22 13:29:10 +0100

    modifications layout pour être plus esthétique

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,20 +21,20 @@ export default function RootLayout({
   return (
     // Couleur du background du site et couleurs permettant de styliser le background
     <html className="!scroll-smooth" lang="en">
-      <body className={`${inter.className} bg-sky-100 text-gray-950 relative h-[5000px] pt-28
-       sm:pt-36 `}>
+      <body className={`${inter.className} bg-sky-100 text-gray-950 relative h-full pt-28
+       sm:pt-36  `}>
         
         <div className="bg-blue-400 absolute top-[-6rem] right-[11rem] h-[31.25rem] 
         w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-gray-400  absolute top-[-1rem] left-[-35rem] h-[31.25rem] 
+        {/* <div className="bg-gray-400  absolute top-[-1rem] left-[-35rem] h-[31.25rem] 
         w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem]
-        xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        xl:left-[-15rem] 2xl:left-[-5rem]"></div> */}
 
-        <div className="bg-gray-400 absolute  right-[11rem] h-[10rem] 
-        w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        {/* <div className="bg-gray-400 absolute  right-[11rem] h-[10rem] 
+        w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div> */}
         <div className="bg-blue-400  absolute bottom-[-0.5rem] left-[-35rem] h-[10rem] 
         w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem]
-        xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        xl:left-[-15rem] 2xl:left-[8rem]"></div>
         <LanguageSelector/>
         <Header/>
         {children}
